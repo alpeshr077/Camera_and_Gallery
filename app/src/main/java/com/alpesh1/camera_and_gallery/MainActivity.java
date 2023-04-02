01dbfe0fdcf5bb1a21a79e5e95627ec31cba557a
Alpesh Rathava <alpeshrathava301@gmail.com>
2023-04-02 22:10:18 +0530

    Initial commit

---
+++ app/src/main/java/com/alpesh1/camera_and_gallery/MainActivity.java
@@ -0,0 +1,55 @@
+package com.alpesh1.camera_and_gallery;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import kotlinx.coroutines.ObsoleteCoroutinesApi;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image12;
+    Button Submit;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image12 = findViewById(R.id.image12);
+        Submit = findViewById(R.id.Submit);
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,10);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == 10) {
+
+                Uri uri = data.getData();
+                image12.setImageURI(uri);
+            }
+        }
+    }
+}
